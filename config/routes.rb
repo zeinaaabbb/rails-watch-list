a3d9da7f20d3661f67ef47980758637ed299cacc
Zeinab Warsama <swarsama@hotmail.co.uk>
2023-11-19 17:10:52 +0000

    removed from routes double quotations and on database yml removed production previous info

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "lists#index"
+  root 'lists#index'
 
   resources :lists do
     resources :bookmarks, only: [:new, :create]
